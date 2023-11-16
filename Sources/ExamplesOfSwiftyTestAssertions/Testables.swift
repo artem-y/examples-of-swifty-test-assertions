c8d61172bcd3d054de545ba3ffddc9c9df90dd8e
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-11-16 17:32:15 +0200

    feat: add example for error throwing assertion

--- Sources/ExamplesOfSwiftyTestAssertions/Testables.swift
+++ Sources/ExamplesOfSwiftyTestAssertions/Testables.swift
@@ -6,3 +6,18 @@ extension [String] {
         filter { !$0.isEmpty }
     }
 }
+
+extension Array {
+    enum Error: Swift.Error, Equatable {
+        case indexOutOfRange
+    }
+
+    /// Safely removes an element from the array.
+    /// - parameter index: Index of element to remove.
+    /// - returns: Removed element.
+    /// - throws: Index out of range error, when given index is out of bounds of the array.
+    mutating func removeElement(at index: Index) throws -> Element {
+        guard indices.contains(index) else { throw Error.indexOutOfRange }
+        return remove(at: index)
+    }
+}
--- Tests/examples-of-swifty-test-ExamplesOfSwiftyTestAssertionsTests/ExamplesOfSwiftyTestAssertionsTests.swift
+++ Tests/examples-of-swifty-test-ExamplesOfSwiftyTestAssertionsTests/ExamplesOfSwiftyTestAssertionsTests.swift
@@ -16,4 +16,16 @@ final class Examples: XCTestCase {
         // Then
         AssertEmpty(result)
     }
+
+    // MARK: - Assert throwsError
+
+    func test_removeElement_atIndexOutOfBounds_throwsIndexOutOfRangeError() {
+        // Given
+        var sut = [1, 2, 3, 4, 5, 6]
+
+        Assert(
+            try sut.removeElement(at: 8), // When
+            throwsError: Array<Int>.Error.indexOutOfRange // Then
+        )
+    }
 }
